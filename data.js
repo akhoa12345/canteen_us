0a9fa654c5cdb6225364b47730eb7fa51969c59f
19120548 <phunganhkhoa123@gmail.com>
2022-12-09 14:16:33 +0700

    update

---
+++ data.js
@@ -0,0 +1,32 @@
+{
+    {
+        "id": 1,
+        "name": "sanpham1",
+        "price": 10000
+    },
+    {
+        "id": 2,
+        "name": "sanpham2",
+        "price": 10000
+    },
+    {
+        "id": 3,
+        "name": "sanpham3",
+        "price": 10000
+    },
+    {
+        "id": 4,
+        "name": "sanpham4",
+        "price": 10000
+    },
+    {
+        "id": 5,
+        "name": "sanpham5",
+        "price": 10000
+    },
+    {
+        "id": 6,
+        "name": "sanpham6",
+        "price": 10000
+    },
+}
